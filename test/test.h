e486f7f12f5cda3f07e6a706c027c76fc73b811f
Josh <josh.benwaa@gmail.com>
2017-07-24 13:59:32 -0500

    DAC
    Added DAC (LTC1592) projects in repo

--- test/test.h
+++ test/test.h
@@ -1,12 +1,12 @@
 #include <24EP256GP202.h>
-#device ICSP=1
-#use delay(clock=140MHz,crystal=10MHz)
-
-#FUSES NOWDT                    //No Watch Dog Timer
+#device icd = true
+#device icsp = 1
+#FUSES NOWDT, FRC                   //No Watch Dog Timer
+#USE DELAY(rc = 8000000)
 #FUSES NOJTAG                   //JTAG disabled
 #FUSES CKSFSM                   //Clock Switching is enabled, fail Safe clock monitor is enabled
 
-
+#PIN_SELECT REFCLK = PIN_B6
 #define LED PIN_A0
 #define DELAY 1
 
